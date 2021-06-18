80c33743a24b1797ca9f59bc7d57db17af492c09
Bhupendra Gehlot <buntydgeneration@gmail.com>
2021-06-18 22:36:36 +0530

    new

---
+++ June 2021/Range Sum Query - Mutable.cpp
@@ -0,0 +1,46 @@
+class NumArray
+{
+public:
+    vector<int> bittree, num;
+    void init(int i, int val)
+    {
+        i = i + 1;
+        while (i <= bittree.size() - 1)
+        {
+            bittree[i] += val;
+            i += i & (-i);
+        }
+    }
+    int getsum(int i)
+    {
+        i = i + 1;
+        int sum = 0;
+        while (i > 0)
+        {
+            sum += bittree[i];
+            i -= i & (-i);
+        }
+        return sum;
+    }
+    NumArray(vector<int> &nums)
+    {
+        num = nums;
+        bittree.resize(nums.size() + 1, 0);
+        for (int i = 0; i < nums.size(); i++)
+        {
+            init(i, nums[i]);
+        }
+    }
+
+    void update(int i, int val)
+    {
+        int diff = val - num[i];
+        num[i] = val;
+        init(i, diff);
+    }
+
+    int sumRange(int i, int j)
+    {
+        return getsum(j) - getsum(i - 1);
+    }
+};
